752f1002fb3fa90466ef62a97e809587a2d5bfdc
p279495 <l.m.van.der.heide@rug.nl>
2019-11-01 11:13:42 +0100

    Better?

---
+++ Wegennet/Main.cpp
@@ -0,0 +1,28 @@
+
+int main() 
+{
+	//should be inputs: road network configurations (set of vertices, arcs (directed), OD-pairs, constant for traffic time per arc)
+	//should be inputs: data for maintenance projects (location sets, durations, reduction of cap, time frame)
+	//Also: write location
+
+
+	//read the data file with road network
+
+	//read data file with maintenance action info
+
+
+	//class for all things with maintenance (all location sets, all durations, ..)
+	//class for all things with road network (matrix link basic times, ?)
+
+
+	//class? with schedule / capacities per link per time unit?
+
+	//calculate equilibrium with no maintenance scheduled.
+	//look at what the Honours boys did..
+
+
+	//Make initial schedule solution 
+
+	//implement a heuristic GA/ALNS?
+
+}
