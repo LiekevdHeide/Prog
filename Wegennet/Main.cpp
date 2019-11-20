8d78947dc08f038f7f7c482d7b0cb050907af0ee
p279495 <l.m.van.der.heide@rug.nl>
2019-11-20 17:07:24 +0100

    Starting again

--- Wegennet/Main.cpp
+++ Wegennet/Main.cpp
@@ -1,21 +1,37 @@
+#include "performanceTimer.h"
+
+#include <vector>
+//#include <functional> //if want to make a standard function that can be different functions (such as policies etc)
+//#include <random>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() 
 {
-	//should be inputs: road network configurations (set of vertices, arcs (directed), OD-pairs, constant for traffic time per arc)
-	//should be inputs: data for maintenance projects (location sets, durations, reduction of cap, time frame)
-	//Also: write location
+	string whichComputer{ "X:/My Documents/Wegennetwerk/Prog" };
+	//string whichComputer{ "C:/Users/lieke/Documents/Wegennetwerk/Prog" };
 
+	Timer time;
 
-	//read the data file with road network
+	//read the data file with road network    //should be inputs: road network configurations (set of vertices, arcs (directed), OD-pairs, constant for traffic time per arc)
+	string roadInput = whichComputer + "/roadInput.txt";
 
-	//read data file with maintenance action info
+	//read data file with maintenance action info    //should be inputs: data for maintenance projects (location sets, durations, reduction of cap, time frame)
+	string maintenanceInput = whichComputer + "/maintenanceInput.txt";
+	
+	//write results to:
+	ofstream write(whichComputer + "/Results.txt");  //, std::ios::app for adding to end of file
 
+	//initialize class for both inputs + read inputs into them
 
 	//class for all things with maintenance (all location sets, all durations, ..)
 	//class for all things with road network (matrix link basic times, ?)
-
-
 	//class? with schedule / capacities per link per time unit?
+
+	cout << "Reading time: " << time.elapsed() << " seconds \n";
+	time.reset();
 
 	//calculate equilibrium with no maintenance scheduled.
 	//look at what the Honours boys did..
@@ -25,4 +41,5 @@ int main()
 
 	//implement a heuristic GA/ALNS?
 
+	cout << "\nImplementation time: " << time.elapsed() << " seconds\n";
 }
--- Wegennet/RoadNetwork.h
+++ Wegennet/RoadNetwork.h
@@ -10,11 +10,15 @@ public:
 	size_t vertices; //vertices in the network
 
 	//OD pairs
+	size_t numberODpairs;
+	vector<size_t> ODpairs;
 
 	//OD demands
+	vector<double> ODdemands; //demand per OD pair
 
 	//paths per OD pair
-
+	vector<size_t> numberODpaths; // how many paths per OD pair (also size of the next..?)
+	vector<vector<size_t>> ODpaths;//which path, order of vertices
 	//
 	vector<vector<double>> standardCapacities;
 	vector<vector<double>> freeFlowTimes;
